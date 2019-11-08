a22bf641d89e9e2ab19e290d56ca7afe678301f1
allison cortez <allizetorc@gmail.com>
2019-11-08 22:42:54 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,4 +6,18 @@ class Patient
     @name = name 
   end
   
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
+  
+  def appointments
+     Appointment.all.select do |appointment|
+       appointment.patient == self
+     end
+  end
+  
+  def doctors
+  
+  end
+  
 end
